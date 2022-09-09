4d9dc5f69dc23005afd92ba71d19d479c5cef9bc
adam4521 <akjaworski@gmail.com>
2022-09-09 22:30:56 +0100

    Update text methods

--- ex3.py
+++ ex3.py
@@ -8,76 +8,112 @@ from pygame.locals import *
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
 GRAY = (150, 150, 150)
 SCREEN = (640,480)
 SCOPE_BOX = (540,480)
 CONTROLS_BOX = (100,480)
 BUTTON_SIZE = (100,50) 
+TEXT_SIZE = (100,12)
 
 pygame.init()
 pygame.display.set_caption('pqm-daisy')
 screen = pygame.display.set_mode(SCREEN)
 
-drawing = False
+strings = ['Ready.', 'This.', 'is', 'some', 'test text', '12345678']
+
+# update text message strings, called periodically
+def set_text_strings(texts, strings):
+    i=0
+    for s in strings:
+        texts[i].set_text(s)
+        i=i+1
+
+
+
+# for button in buttons:
+buttons = []
+for s in ['Run/Stop', 'Mode', 'Logging', 'Scales', 'Options', 'About']:
+    button = thorpy.make_button(s)
+    button.set_size(BUTTON_SIZE)
+    buttons.append(button)
+
+
+texts = []
+for s in range(0,7):
+    text = thorpy.make_text('1234567890')
+    text.set_size(TEXT_SIZE)
+    texts.append(text)
+
+uibox = thorpy.Box(elements=[*buttons, *texts])
+
+
+menu = thorpy.Menu(uibox) #create a menu for auto events handling
+for element in menu.get_population():
+    element.surface = screen
+
+uibox.set_size(CONTROLS_BOX)
+uibox.set_topleft((SCREEN[0]-CONTROLS_BOX[0],0))
+#menu.play() #launch the menu
+
+
+def get_capture(points1, points2, points3):
+    for t in range(0, SCOPE_BOX[0]):
+        points1.append((t, 50*math.sin(t*math.pi/180) + 20*(random.random() - 0.5)))
+        points2.append((t, 50*math.sin(t*math.pi/180) + 20*(random.random() - 0.5)))
+        points3.append((t, points1[t][1]*points2[t][1]))
+    return points1, points2, points3
+
+
+def to_screen_coordinates(points1, points2, points3):
+    plot1 = []
+    plot2 = []
+    plot3 = []
+    for t in range(0, len(points1)):
+        # invert y axis in plot coordinates, which increase from top of the display downwards
+        plot1.append((t, 100-int(points1[t][1])))
+        plot2.append((t, 400-int(points2[t][1])))
+        plot3.append((t, 250-int(0.05*points3[t][1])))
+    return plot1, plot2, plot3
+
+
+capturing = True
 running = True
 frames = 0
 seconds = int(time.time())
 
-
-button1 = thorpy.make_button("Summary")
-button2 = thorpy.make_button("Scope")
-button3 = thorpy.make_button("FFT")
-button4 = thorpy.make_button("Options")
-button5 = thorpy.make_button("Unused")
-button6 = thorpy.make_button("Unused")
-
-buttons = [button1, button2, button3, button4, button5, button6]
-for button in buttons:
-    button.set_size(BUTTON_SIZE)
-
-box = thorpy.Box(elements=buttons)
-
-menu = thorpy.Menu(box) #create a menu for auto events handling
-for element in menu.get_population():
-    element.surface = screen
-
-box.set_size(CONTROLS_BOX)
-box.set_topleft((SCREEN[0]-CONTROLS_BOX[0],0))
-#menu.play() #launch the menu
-
-
-
-
 while running:
 
-    screen.fill(GRAY)
-
+    # deal with the event queue
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == pygame.K_q):
             running = False
         menu.react(event)
 
-    points1 = []
-    points2 = []
-    for t in range(0, SCOPE_BOX[0]):
-        points1.append((t, 100+int(50*math.sin(t*math.pi/180)) + 30*(random.random() - 0.5)))
-        points2.append((t, 300+int(50*math.sin(t*math.pi/180)) + 30*(random.random() - 0.5)))
+    # update line data
+    if capturing:
+        plot1, plot2, plot3 = to_screen_coordinates(*get_capture([],[],[]))
 
-
-    pygame.draw.lines(screen, RED, False, points1, 2)
-    pygame.draw.lines(screen, BLUE, False, points2, 2)
-    box.blit()
-    # button1.update()
+    # regenerate display 
+    screen.fill(GRAY)
+    pygame.draw.lines(screen, RED, False, plot1, 2)
+    pygame.draw.lines(screen, BLUE, False, plot2, 2)
+    pygame.draw.lines(screen, MAGENTA, False, plot3, 2)
+    uibox.blit()
+    # uibox.update()
     
-    pygame.display.update()
     frames = frames + 1
-
     newtime = int(time.time())
+
+    # refresh the information box every second
     if newtime != seconds:
         seconds = newtime
-        print("FPS: " + str(frames))
+        strings[1] = "FPS: " + str(frames)
+        set_text_strings(texts, strings)
         frames = 0
 
+
+    pygame.display.update()
 
 pygame.quit()
 
